5f5199f2adee8c513a95d053570c8062f780c365
Krzysztof Ciesielski <krzysztof.ciesielski@softwaremill.com>
2014-12-30 18:11:51 +0100

    Implement rule 3.6 - After the Subscription is cancelled, additional Subscription.request(long n) MUST be NOPs

--- src/main/scala/com/softwaremill/react/kafka/KafkaTopicSubscription.scala
+++ src/main/scala/com/softwaremill/react/kafka/KafkaTopicSubscription.scala
@@ -1,22 +1,23 @@
 package com.softwaremill.react.kafka
 
+import com.softwaremill.react.kafka.RichKafkaConsumer._
 import kafka.consumer.KafkaConsumer
-import org.reactivestreams.{Subscription, Subscriber}
+import org.reactivestreams.{Subscriber, Subscription}
 
 import scala.util.control.NonFatal
-
 private[kafka] class KafkaTopicSubscription(consumer: KafkaConsumer,
                                             subscriber: Subscriber[_ >: String]) extends Subscription {
 
-  override def request(n: Long): Unit = {
-    require(n > 0, "n <= 0")
-
-    (1L to n.toInt) foreach { _ =>
-      consumer.read(bytes => {
-        val msgAsStr = new String(bytes)
-        subscriber.onNext(msgAsStr)
-      })
-    }
+  override def request(n: Long) {
+    require(n > 0, "Rule 3.9: n <= 0")
+    if (consumer.connected()) {
+      (1L to n.toInt) foreach { _ =>
+        consumer.read(bytes => {
+          val msgAsStr = new String(bytes)
+          subscriber.onNext(msgAsStr)
+        })
+      }
+    } // else 3.6 NOP
   }
 
   override def cancel() = try {
