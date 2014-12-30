f2f9650174597fa39433b44f501532e236cdb43c
Krzysztof Ciesielski <krzysztof.ciesielski@softwaremill.com>
2014-12-30 16:28:46 +0100

    Extract base stuff for the subscriber blackbox verification

---
+++ src/test/scala/com/softwaremill/react/kafka/ReactiveKafkaSubscriberBlackboxSpec.scala
@@ -0,0 +1,32 @@
+package com.softwaremill.react.kafka
+
+import akka.stream.scaladsl.{PublisherSink, Source}
+import org.reactivestreams.{Publisher, Subscriber}
+import org.reactivestreams.tck.{SubscriberBlackboxVerification, TestEnvironment}
+import org.scalatest.testng.TestNGSuiteLike
+
+import scala.concurrent.duration.{FiniteDuration, _}
+import scala.language.postfixOps
+
+class ReactiveKafkaSubscriberBlackboxSpec(defaultTimeout: FiniteDuration)
+  extends SubscriberBlackboxVerification[String](new TestEnvironment(defaultTimeout.toMillis))
+  with TestNGSuiteLike with ReactiveStreamsTckVerificationBase {
+
+  def this() = this(300 millis)
+
+  override def createSubscriber(): Subscriber[String] = {
+    kafka.publish("kci", "grupa")
+  }
+
+  def createHelperSource(elements: Long): Source[String] = elements match {
+    case 0 => Source.empty()
+    case Long.MaxValue => Source(() => List(message).iterator)
+    case n if n <= Int.MaxValue => Source(List.fill(n.toInt)(message))
+    case n => sys.error("n > Int.MaxValue")
+  }
+
+  def createHelperPublisher(elements: Long): Publisher[String] = {
+    createHelperSource(elements).runWith(PublisherSink())
+  }
+
+}
---
+++ src/test/scala/com/softwaremill/react/kafka/ReactiveStreamsTckVerificationBase.scala
@@ -0,0 +1,15 @@
+package com.softwaremill.react.kafka
+
+import akka.actor.ActorSystem
+import akka.stream.FlowMaterializer
+
+trait ReactiveStreamsTckVerificationBase {
+
+  implicit val system = ActorSystem()
+  implicit val mat = FlowMaterializer()
+
+  val kafka = new ReactiveKafka("192.168.0.10:9092")
+
+  val message = "foo"
+}
+
