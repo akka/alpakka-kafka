92f761d3c9468faf9ac1b72917809f6772ffeb8f
Krzysztof Ciesielski <krzysztof.ciesielski@softwaremill.com>
2015-01-02 16:00:33 +0100

    Don't send anything to the producer when receiving next element via the consumer

--- src/main/scala/com/softwaremill/react/kafka/ReactiveKafkaSubscriber.scala
+++ src/main/scala/com/softwaremill/react/kafka/ReactiveKafkaSubscriber.scala
@@ -25,7 +25,6 @@ private[kafka] class ReactiveKafkaSubscriber(val producer: KafkaProducer) extend
   }
 
   override def onNext(msg: String) {
-    producer.send(msg)
     active.get().request(1)
   }
 
