03697eb68bafb66fe7ec9cea329bdcd2d37c2a07
Krzysztof Ciesielski <krzysztof.ciesielski@softwaremill.com>
2015-04-09 14:26:59 +0200

    Keep polling on queues when there's pending demand

--- src/main/scala/com/softwaremill/react/kafka/KafkaActorPublisher.scala
+++ src/main/scala/com/softwaremill/react/kafka/KafkaActorPublisher.scala
@@ -1,16 +1,19 @@
 package com.softwaremill.react.kafka
 
 import akka.stream.actor.{ActorPublisher, ActorPublisherMessage}
+import com.softwaremill.react.kafka.KafkaActorPublisher.Poll
 import kafka.consumer.{ConsumerTimeoutException, KafkaConsumer}
 import kafka.serializer.Decoder
 
+import scala.concurrent.duration._
+import scala.language.postfixOps
 import scala.util.{Failure, Success, Try}
 private[kafka] class KafkaActorPublisher[T](consumer: KafkaConsumer, decoder: Decoder[T]) extends ActorPublisher[T] {
 
   val iterator = consumer.iterator()
 
   override def receive = {
-    case ActorPublisherMessage.Request(_) => read()
+    case ActorPublisherMessage.Request(_) | Poll => read()
     case ActorPublisherMessage.Cancel | ActorPublisherMessage.SubscriptionTimeoutExceeded => cleanupResources()
   }
 
@@ -28,19 +31,24 @@ private[kafka] class KafkaActorPublisher[T](consumer: KafkaConsumer, decoder: De
   }
 
   private def readDemandedItems() {
-    var maybeMoreElements = true
-    while (isActive && totalDemand > 0 && maybeMoreElements) {
+    while (isActive && totalDemand > 0) {
       tryReadingSingleElement() match {
-        case Success(None) => maybeMoreElements = false // No more elements
+        case Success(None) =>
+          if (totalDemand > 0)
+            self ! Poll
         case Success(valueOpt) =>
           valueOpt.foreach(element => onNext(element))
         case Failure(ex) => onError(ex)
       }
     }
-
   }
+
   private def cleanupResources() {
     consumer.close()
     context.stop(self)
   }
+}
+
+private[kafka] object KafkaActorPublisher {
+  case object Poll
 }
