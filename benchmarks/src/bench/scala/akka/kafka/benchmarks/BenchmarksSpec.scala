7ad1f0781a1455d659ae9b41d4c6f0fde6927e66
Krzysztof Ciesielski <krzysztof.ciesielski@softwaremill.com>
2016-07-19 12:44:58 +0200

    Set some tests in scalatest runner

--- benchmarks/src/bench/scala/akka/kafka/benchmarks/BenchmarksSpec.scala
+++ benchmarks/src/bench/scala/akka/kafka/benchmarks/BenchmarksSpec.scala
@@ -9,14 +9,26 @@ import akka.kafka.benchmarks.app.RunTestCommand
 import akka.stream.ActorMaterializer
 import akka.testkit.TestKit
 import org.scalatest.{BeforeAndAfterAll, FlatSpecLike}
+import scala.concurrent.Await
+import scala.concurrent.duration.FiniteDuration
 import scala.language.postfixOps
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
 
 class BenchmarksSpec extends TestKit(ActorSystem("AkkaKafkaBenchmarks")) with FlatSpecLike with BeforeAndAfterAll {
+
+  val kafkaHost = "localhost:9092"
+  val timeout: FiniteDuration = 1 hour
 
   implicit val mat = ActorMaterializer()
 
   it should "work" in {
-    Benchmarks.run(RunTestCommand("all", "localhost:9092", 100000, 200000, 25000))
+    Await.result(
+    for {
+      _ <- Benchmarks.run(RunTestCommand("plain-consumer", kafkaHost, 10000000, 20000000, 2500000))
+      _ <- Benchmarks.run(RunTestCommand("akka-plain-consumer", kafkaHost, 10000000, 20000000, 2500000))
+    } yield ()
+    , timeout)
   }
 
   override protected def afterAll(): Unit = {
