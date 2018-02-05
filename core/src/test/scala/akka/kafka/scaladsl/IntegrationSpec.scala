f3d6ce9d35ae1680c943ab5a195fca7eb739f13c
sullis <github@seansullivan.com>
2018-02-05 04:35:08 -0800

    upgrade from SBT 0.13.16 to SBT 1.1.0 (#404)

--- core/src/test/scala/akka/kafka/scaladsl/IntegrationSpec.scala
+++ core/src/test/scala/akka/kafka/scaladsl/IntegrationSpec.scala
@@ -2,6 +2,7 @@
  * Copyright (C) 2014 - 2016 Softwaremill <http://softwaremill.com>
  * Copyright (C) 2016 Lightbend Inc. <http://www.lightbend.com>
  */
+
 package akka.kafka.scaladsl
 
 import java.util.UUID
