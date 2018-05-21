93ce5f9e11d1ab16d1130f9538d85d73b3a230a3
Enno Runne <458526+ennru@users.noreply.github.com>
2018-05-21 15:29:10 +0200

    Collect used ports in one place

---
+++ core/src/test/scala/akka/kafka/KafkaPorts.scala
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2014 - 2016 Softwaremill <http://softwaremill.com>
+ * Copyright (C) 2016 - 2018 Lightbend Inc. <http://www.lightbend.com>
+ */
+
+package akka.kafka.scaladsl
+
+/**
+ * Ports to use for Kafka and Zookeeper throughout integration tests.
+ * Zookeeper is Kafka port + 1 if nothing else specified.
+ */
+object KafkaPorts {
+
+  val IntegrationSpec = 9002
+  val RetentionPeriodSpec = 9012
+  val TransactionsSpec = 9022
+
+}
