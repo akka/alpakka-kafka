31bcf69737429e419ae620f2178af94661e021fc
Enno Runne <458526+ennru@users.noreply.github.com>
2018-10-30 10:33:56 +0100

    Testkit factory classes for classes created in Alpakka Kafka stages

---
+++ testkit/src/main/scala/akka/kafka/akka/kafka/testkit/ConsumerResultFactory.scala
@@ -0,0 +1,54 @@
+package akka.kafka.akka.kafka.testkit
+
+import java.util.concurrent.CompletionStage
+
+import akka.Done
+import akka.annotation.ApiMayChange
+import akka.kafka.ConsumerMessage
+import akka.kafka.ConsumerMessage.{CommittableOffset, GroupTopicPartition, PartitionOffset}
+import org.apache.kafka.clients.consumer.ConsumerRecord
+
+import scala.compat.java8.FutureConverters._
+import scala.concurrent.Future
+
+/**
+ * Factory methods to create instances that normally are emitted by [[akka.kafka.scaladsl.Consumer]] and [[akka.kafka.javadsl.Consumer]] flows.
+ */
+@ApiMayChange
+object ConsumerResultFactory {
+
+  def partitionOffset(groupId: String, topic: String, partition: Int, offset: Long): ConsumerMessage.PartitionOffset =
+    ConsumerMessage.PartitionOffset(ConsumerMessage.GroupTopicPartition(groupId, topic, partition), offset)
+
+  def partitionOffset(key: GroupTopicPartition, offset: Long) = ConsumerMessage.PartitionOffset(key, offset)
+
+  def committableOffset(groupId: String,
+                        topic: String,
+                        partition: Int,
+                        offset: Long,
+                        metadata: String): ConsumerMessage.CommittableOffset =
+    committableOffset(partitionOffset(groupId, topic, partition, offset), metadata)
+
+  def committableOffset(partitionOffset: ConsumerMessage.PartitionOffset,
+                        metadata: String): ConsumerMessage.CommittableOffset = {
+    val metadata2 = metadata
+    val partitionOffset2 = partitionOffset
+    new ConsumerMessage.CommittableOffsetMetadata {
+      override val metadata: String = metadata2
+      override val partitionOffset: ConsumerMessage.PartitionOffset = partitionOffset2
+      override def commitScaladsl(): Future[Done] = Future.successful(Done)
+      override def commitJavadsl(): CompletionStage[Done] = commitScaladsl().toJava
+    }
+  }
+
+  def committableMessage[K, V](
+      record: ConsumerRecord[K, V],
+      committableOffset: CommittableOffset
+  ): ConsumerMessage.CommittableMessage[K, V] = ConsumerMessage.CommittableMessage(record, committableOffset)
+
+  def transactionalMessage[K, V](
+      record: ConsumerRecord[K, V],
+      partitionOffset: PartitionOffset
+  ): ConsumerMessage.TransactionalMessage[K, V] = ConsumerMessage.TransactionalMessage(record, partitionOffset)
+
+}
---
+++ testkit/src/main/scala/akka/kafka/akka/kafka/testkit/ProducerResultFactory.scala
@@ -0,0 +1,36 @@
+package akka.kafka.akka.kafka.testkit
+
+import akka.annotation.ApiMayChange
+import akka.kafka.ProducerMessage
+import org.apache.kafka.clients.producer.{ProducerRecord, RecordMetadata}
+
+import scala.collection.immutable
+import scala.collection.JavaConverters._
+
+/**
+ * Factory methods to create instances that normally are emitted by [[akka.kafka.scaladsl.Producer]] and [[akka.kafka.javadsl.Producer]] flows.
+ */
+@ApiMayChange
+object ProducerResultFactory {
+
+  def result[K, V, PassThrough](
+      metadata: RecordMetadata,
+      message: ProducerMessage.Message[K, V, PassThrough]
+  ): ProducerMessage.Result[K, V, PassThrough] = ProducerMessage.Result(metadata, message)
+
+  def multiResultPart[K, V](
+      metadata: RecordMetadata,
+      record: ProducerRecord[K, V]
+  ): ProducerMessage.MultiResultPart[K, V] = ProducerMessage.MultiResultPart(metadata, record)
+
+  def multiResult[K, V, PassThrough](
+      parts: immutable.Seq[ProducerMessage.MultiResultPart[K, V]],
+      passThrough: PassThrough
+  ): ProducerMessage.MultiResult[K, V, PassThrough] = ProducerMessage.MultiResult(parts, passThrough)
+
+  /** Java API */
+  def multiResult[K, V, PassThrough](
+      parts: java.util.Collection[ProducerMessage.MultiResultPart[K, V]],
+      passThrough: PassThrough
+  ): ProducerMessage.MultiResult[K, V, PassThrough] = ProducerMessage.MultiResult(parts.asScala.toList, passThrough)
+}
