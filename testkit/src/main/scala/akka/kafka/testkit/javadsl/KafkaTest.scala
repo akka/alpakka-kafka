58d3b70990539bf0f0c83bf39c81eac57fa50a51
Enno Runne <458526+ennru@users.noreply.github.com>
2018-11-29 12:51:23 +0100

    Some 2.11 dance

--- testkit/src/main/scala/akka/kafka/testkit/javadsl/KafkaTest.scala
+++ testkit/src/main/scala/akka/kafka/testkit/javadsl/KafkaTest.scala
@@ -8,6 +8,7 @@ package akka.kafka.testkit.javadsl
 import java.util.concurrent.{CompletionStage, TimeUnit}
 
 import akka.Done
+import akka.japi.Pair
 import akka.kafka.Subscriptions
 import akka.kafka.javadsl.Consumer
 import akka.kafka.scaladsl.Producer
@@ -17,6 +18,7 @@ import akka.stream.javadsl.{Keep, Sink}
 import akka.stream.scaladsl.Source
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.kafka.clients.producer.ProducerRecord
+import org.scalatest.run
 import org.slf4j.{Logger, LoggerFactory}
 
 import scala.compat.java8.FutureConverters._
@@ -44,13 +46,22 @@ abstract class KafkaTest extends KafkaTestKit {
       .runWith(Producer.plainSink(producerDefaults))(materializer)
       .toJava
 
-  protected def consumeString(topic: String,
-                              take: Long): Consumer.DrainingControl[java.util.List[ConsumerRecord[String, String]]] =
+  private final type Records = java.util.List[ConsumerRecord[String, String]]
+
+  protected def consumeString(topic: String, take: Long): Consumer.DrainingControl[Records] =
     Consumer
       .plainSource(consumerDefaults.withGroupId(createGroupId(1)), Subscriptions.topics(topic))
       .take(take)
-      .toMat(Sink.seq, Keep.both[Consumer.Control, CompletionStage[java.util.List[ConsumerRecord[String, String]]]])
-      .mapMaterializedValue(p => Consumer.createDrainingControl(p))
+      .toMat(Sink.seq, Keep.both[Consumer.Control, CompletionStage[Records]])
+      .mapMaterializedValue(
+        new akka.japi.function.Function[Pair[Consumer.Control, CompletionStage[Records]], Consumer.DrainingControl[
+          Records
+        ]] {
+          override def apply(
+              p: Pair[Consumer.Control, CompletionStage[Records]]
+          ): Consumer.DrainingControl[Records] = Consumer.createDrainingControl(p)
+        }
+      )
       .run(materializer)
 
   @throws[Exception]
