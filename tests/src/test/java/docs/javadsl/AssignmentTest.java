4e7dbd831a1f021358a8612ec70df6811ea0942a
Enno Runne <458526+ennru@users.noreply.github.com>
2019-01-16 14:10:53 +0100

    Make pattern topic subscription test order independent

--- tests/src/test/java/docs/javadsl/AssignmentTest.java
+++ tests/src/test/java/docs/javadsl/AssignmentTest.java
@@ -10,6 +10,7 @@ import akka.actor.ActorSystem;
 import akka.kafka.AutoSubscription;
 import akka.kafka.KafkaPorts;
 import akka.kafka.ManualSubscription;
+import akka.kafka.ProducerMessage;
 import akka.kafka.Subscriptions;
 import akka.kafka.javadsl.Consumer;
 // #testkit
@@ -34,7 +35,6 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.CompletionStage;
-import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -54,8 +54,7 @@ public class AssignmentTest extends EmbeddedKafkaJunit4Test {
   // #testkit
 
   @Test
-  public void mustConsumeFromTheSpecifiedSingleTopic()
-      throws ExecutionException, InterruptedException {
+  public void mustConsumeFromTheSpecifiedSingleTopic() throws Exception {
     final String topic = createTopic(0, 1, 1);
     final String group = createGroupId(0);
     final Integer totalMessages = 100;
@@ -64,7 +63,7 @@ public class AssignmentTest extends EmbeddedKafkaJunit4Test {
             .map(msg -> new ProducerRecord<>(topic, 0, DefaultKey(), msg.toString()))
             .runWith(Producer.plainSink(producerDefaults()), mat);
 
-    producerCompletion.toCompletableFuture().get();
+    resultOf(producerCompletion);
 
     // #single-topic
     final AutoSubscription subscription = Subscriptions.topics(topic);
@@ -73,59 +72,50 @@ public class AssignmentTest extends EmbeddedKafkaJunit4Test {
     // #single-topic
 
     final Integer receivedMessages =
-        consumer
-            .takeWhile(m -> Integer.valueOf(m.value()) < totalMessages, true)
-            .runWith(Sink.seq(), mat)
-            .toCompletableFuture()
-            .get()
+        resultOf(
+                consumer
+                    .takeWhile(m -> Integer.valueOf(m.value()) < totalMessages, true)
+                    .runWith(Sink.seq(), mat))
             .size();
 
     assertEquals(totalMessages, receivedMessages);
   }
 
   @Test
-  public void mustConsumeFromTheSpecifiedTopicPattern()
-      throws ExecutionException, InterruptedException {
-    final List<String> topics = Arrays.asList(createTopic(1, 1, 1), createTopic(1, 1, 1));
+  public void mustConsumeFromTheSpecifiedTopicPattern() throws Exception {
+    final List<String> topics = Arrays.asList(createTopic(9001, 1, 1), createTopic(9002, 1, 1));
     final String group = createGroupId(0);
     final Integer totalMessages = 100;
     final CompletionStage<Done> producerCompletion =
         Source.range(1, totalMessages)
-            .mapConcat(
+            .map(
                 msg ->
-                    topics
-                        .stream()
-                        .map(t -> new ProducerRecord<>(t, 0, DefaultKey(), msg.toString()))
-                        .collect(Collectors.toList()))
-            .concat(
-                Source.single(
-                    new ProducerRecord<>(
-                        topics.get(0), 0, DefaultKey(), String.valueOf(totalMessages + 1))))
-            .runWith(Producer.plainSink(producerDefaults()), mat);
+                    ProducerMessage.multi(
+                        topics
+                            .stream()
+                            .map(t -> new ProducerRecord<>(t, 0, DefaultKey(), msg.toString()))
+                            .collect(Collectors.toList())))
+            .via(Producer.flexiFlow(producerDefaults()))
+            .runWith(Sink.ignore(), mat);
 
-    producerCompletion.toCompletableFuture().get();
+    resultOf(producerCompletion);
 
     // #topic-pattern
-    final String pattern = "topic-1-[0-9]+";
+    final String pattern = "topic-900[1|2]-[0-9]+";
     final AutoSubscription subscription = Subscriptions.topicPattern(pattern);
     final Source<ConsumerRecord<String, String>, Consumer.Control> consumer =
         Consumer.plainSource(consumerDefaults().withGroupId(group), subscription);
     // #topic-pattern
 
+    int expectedTotal = totalMessages * topics.size();
     final Integer receivedMessages =
-        consumer
-            .takeWhile(m -> Integer.valueOf(m.value()) <= totalMessages)
-            .runWith(Sink.seq(), mat)
-            .toCompletableFuture()
-            .get()
-            .size();
+        resultOf(consumer.take(expectedTotal).runWith(Sink.seq(), mat)).size();
 
-    assertEquals(totalMessages * topics.size(), (int) receivedMessages);
+    assertEquals(expectedTotal, (int) receivedMessages);
   }
 
   @Test
-  public void mustConsumeFromTheSpecifiedPartition()
-      throws ExecutionException, InterruptedException {
+  public void mustConsumeFromTheSpecifiedPartition() throws Exception {
     final String topic = createTopic(2, 2, 1);
     final Integer totalMessages = 100;
     final CompletionStage<Done> producerCompletion =
@@ -137,7 +127,7 @@ public class AssignmentTest extends EmbeddedKafkaJunit4Test {
                 })
             .runWith(Producer.plainSink(producerDefaults()), mat);
 
-    producerCompletion.toCompletableFuture().get();
+    resultOf(producerCompletion);
 
     // #assingment-single-partition
     final Integer partition = 0;
@@ -152,13 +142,12 @@ public class AssignmentTest extends EmbeddedKafkaJunit4Test {
             .take(totalMessages / 2)
             .map(msg -> Integer.valueOf(msg.value()))
             .runWith(Sink.seq(), mat);
-    final List<Integer> messages = consumerCompletion.toCompletableFuture().get();
+    final List<Integer> messages = resultOf(consumerCompletion);
     messages.forEach(m -> assertEquals(0, m % 2));
   }
 
   @Test
-  public void mustConsumeFromTheSpecifiedPartitionAndOffset()
-      throws ExecutionException, InterruptedException {
+  public void mustConsumeFromTheSpecifiedPartitionAndOffset() throws Exception {
     final String topic = createTopic(3, 1, 1);
     final Integer totalMessages = 100;
     final CompletionStage<Done> producerCompletion =
@@ -166,7 +155,7 @@ public class AssignmentTest extends EmbeddedKafkaJunit4Test {
             .map(msg -> new ProducerRecord<>(topic, 0, DefaultKey(), msg.toString()))
             .runWith(Producer.plainSink(producerDefaults()), mat);
 
-    producerCompletion.toCompletableFuture().get();
+    resultOf(producerCompletion);
 
     // #assingment-single-partition-offset
     final Integer partition = 0;
@@ -179,13 +168,12 @@ public class AssignmentTest extends EmbeddedKafkaJunit4Test {
 
     final CompletionStage<List<Long>> consumerCompletion =
         consumer.take(totalMessages / 2).map(ConsumerRecord::offset).runWith(Sink.seq(), mat);
-    final List<Long> messages = consumerCompletion.toCompletableFuture().get();
+    final List<Long> messages = resultOf(consumerCompletion);
     IntStream.range(0, (int) offset).forEach(idx -> assertEquals(idx, messages.get(idx) - offset));
   }
 
   @Test
-  public void mustConsumeFromTheSpecifiedPartitionAndTimestamp()
-      throws ExecutionException, InterruptedException {
+  public void mustConsumeFromTheSpecifiedPartitionAndTimestamp() throws Exception {
     final String topic = createTopic(4, 1, 1);
     final Integer totalMessages = 100;
     final CompletionStage<Done> producerCompletion =
@@ -196,7 +184,7 @@ public class AssignmentTest extends EmbeddedKafkaJunit4Test {
                         topic, 0, System.currentTimeMillis(), DefaultKey(), msg.toString()))
             .runWith(Producer.plainSink(producerDefaults()), mat);
 
-    producerCompletion.toCompletableFuture().get();
+    resultOf(producerCompletion);
 
     // #assingment-single-partition-timestamp
     final Integer partition = 0;
@@ -215,13 +203,7 @@ public class AssignmentTest extends EmbeddedKafkaJunit4Test {
             .map(ConsumerRecord::timestamp)
             .runWith(Sink.seq(), mat);
     final long oldMessages =
-        consumerCompletion
-            .toCompletableFuture()
-            .get()
-            .stream()
-            .map(t -> t - now)
-            .filter(t -> t > 5000)
-            .count();
+        resultOf(consumerCompletion).stream().map(t -> t - now).filter(t -> t > 5000).count();
     assertEquals(0, oldMessages);
   }
 
