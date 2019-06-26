60d09bdad9960e2eda4e25ed71c3fbdb5401bfb1
sstoliarenko <s.stoliarenko@newagesol.com>
2019-06-26 16:06:35 +0300

    Fix incompatibility checking

--- core/src/main/scala/akka/kafka/KafkaConnectionFailed.scala
+++ core/src/main/scala/akka/kafka/KafkaConnectionFailed.scala
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2014 - 2016 Softwaremill <http://softwaremill.com>
+ * Copyright (C) 2016 - 2019 Lightbend Inc. <http://www.lightbend.com>
+ */
+
 package akka.kafka
 
 import org.apache.kafka.common.errors.TimeoutException
