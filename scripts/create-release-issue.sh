7cedb399ac98e5f25c40a6192e98f32d4b7b975a
Enno Runne <458526+ennru@users.noreply.github.com>
2019-09-05 10:07:23 +0200

    Configure autolabeler bot; update release helpers

--- scripts/create-release-issue.sh
+++ scripts/create-release-issue.sh
@@ -6,5 +6,5 @@ then
   echo specify the version name to be released, eg. 1.0.0
 else
   sed -e 's/\$VERSION\$/'$VERSION'/g' docs/release-train-issue-template.md > /tmp/release-$VERSION.md
-  echo Created $(hub issue create -F /tmp/release-$VERSION.md -M $VERSION)
+  echo Created $(hub issue create -F /tmp/release-$VERSION.md --milestone $VERSION --browse)
 fi
