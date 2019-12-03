a5a25a0dba04ad477df0fff91c75d90ad54db956
Enno <458526+ennru@users.noreply.github.com>
2019-12-03 13:25:23 +0100

    Release notes update to call the release 2.0.0-M2 instead (#1000)

--- testkit/src/main/java/akka/kafka/testkit/javadsl/TestcontainersKafkaTest.java
+++ testkit/src/main/java/akka/kafka/testkit/javadsl/TestcontainersKafkaTest.java
@@ -35,18 +35,14 @@ public abstract class TestcontainersKafkaTest extends KafkaTest {
     super(system, materializer, startKafka(settings));
   }
 
-  /**
-   * @deprecated Use constructor with `testcontainersSettings` instead. since 2.0.0
-   */
+  /** @deprecated Use constructor with `testcontainersSettings` instead. since 2.0.0 */
   @Deprecated
   protected TestcontainersKafkaTest(
       ActorSystem system, Materializer materializer, String confluentPlatformVersion) {
     super(system, materializer, startKafka(confluentPlatformVersion));
   }
 
-  /**
-   * @deprecated Use method with `testcontainersSettings` instead. since 2.0.0
-   */
+  /** @deprecated Use method with `testcontainersSettings` instead. since 2.0.0 */
   @Deprecated
   protected static String startKafka(String confluentPlatformVersion) {
     KafkaTestkitTestcontainersSettings settings =
