22856d0fefffd2916831c384ed0dc0501f942603
Sean Glover <sean@seanglover.com>
2019-12-02 21:07:07 -0500

    Alpakka partion assignor

--- tests/src/test/scala/akka/kafka/scaladsl/RebalanceSpec.scala
+++ tests/src/test/scala/akka/kafka/scaladsl/RebalanceSpec.scala
@@ -5,20 +5,25 @@
 
 package akka.kafka.scaladsl
 
-import akka.{Done, NotUsed}
+import java.util
+import java.util.concurrent.atomic.AtomicReference
+
 import akka.kafka._
 import akka.kafka.testkit.scaladsl.TestcontainersKafkaLike
-import akka.stream.scaladsl.{Keep, RunnableGraph, Source}
+import akka.stream.scaladsl.{Keep, Source}
 import akka.stream.testkit.TestSubscriber
 import akka.stream.testkit.scaladsl.StreamTestKit.assertAllStagesStopped
 import akka.stream.testkit.scaladsl.TestSink
 import akka.testkit.TestProbe
-import org.apache.kafka.clients.consumer.{ConsumerConfig, ConsumerRecord}
+import akka.{Done, NotUsed}
+import org.apache.kafka.clients.consumer._
+import org.apache.kafka.clients.consumer.internals.AbstractPartitionAssignor
 import org.apache.kafka.common.TopicPartition
 import org.scalatest._
+import org.slf4j.{Logger, LoggerFactory}
 
-import scala.collection.immutable
 import scala.concurrent.duration._
+import scala.jdk.CollectionConverters._
 import scala.util.Random
 
 class RebalanceSpec extends SpecBase with TestcontainersKafkaLike with Inside {
@@ -39,34 +44,51 @@ class RebalanceSpec extends SpecBase with TestcontainersKafkaLike with Inside {
       val topicSuffix = Random.nextInt()
       val topic1 = createTopic(topicSuffix, partitions = 2)
       val group1 = createGroupId(1)
+      val tp0 = new TopicPartition(topic1, partition0)
+      val tp1 = new TopicPartition(topic1, partition1)
       val consumerSettings = consumerDefaults
         .withProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "500") // 500 is the default value
+        .withProperty(ConsumerConfig.PARTITION_ASSIGNMENT_STRATEGY_CONFIG, classOf[AlpakkaAssignor].getName)
         .withGroupId(group1)
 
       awaitProduce(produce(topic1, 0 to count.toInt, partition1))
 
-      // Subscribe to the topic (without demand)
+      AlpakkaAssignor.clientIdToPartitionMap.set(
+        Map(
+          "consumer-1" -> Set(tp0, tp1)
+        )
+      )
+
+      log.debug("Subscribe to the topic (without demand)")
       val probe1rebalanceActor = TestProbe()
       val probe1subscription = Subscriptions.topics(topic1).withRebalanceListener(probe1rebalanceActor.ref)
       val (control1, probe1) = Consumer
-        .plainSource(consumerSettings, probe1subscription)
+        .plainSource(consumerSettings.withClientId("consumer-1"), probe1subscription)
         .toMat(TestSink.probe)(Keep.both)
         .run()
 
-      // Await initial partition assignment
+      log.debug("Await initial partition assignment")
       probe1rebalanceActor.expectMsg(
         TopicPartitionsAssigned(probe1subscription,
                                 Set(new TopicPartition(topic1, partition0), new TopicPartition(topic1, partition1)))
       )
 
-      // read one message from probe1 with partition 1
-      probe1.requestNext()
+      log.debug("read one message from probe1 with partition 1")
+      val record = probe1.requestNext()
+      log.debug(s"read one msg: $record")
 
-      // Subscribe to the topic (without demand)
+      AlpakkaAssignor.clientIdToPartitionMap.set(
+        Map(
+          "consumer-1" -> Set(tp0),
+          "consumer-2" -> Set(tp1)
+        )
+      )
+
+      log.debug("Subscribe to the topic (without demand)")
       val probe2rebalanceActor = TestProbe()
       val probe2subscription = Subscriptions.topics(topic1).withRebalanceListener(probe2rebalanceActor.ref)
       val (control2, probe2) = Consumer
-        .plainSource(consumerSettings, probe2subscription)
+        .plainSource(consumerSettings.withClientId("consumer-2"), probe2subscription)
         .toMat(TestSink.probe)(Keep.both)
         .run()
 
@@ -84,15 +106,16 @@ class RebalanceSpec extends SpecBase with TestcontainersKafkaLike with Inside {
         TopicPartitionsAssigned(probe2subscription, Set(new TopicPartition(topic1, partition1)))
       )
 
+      log.debug("resume demand on both consumers")
       probe1.request(count)
       probe2.request(count)
 
       val probe2messages = probe2.expectNextN(count)
 
-      // no further messages enqueued on probe1 as partition 1 is balanced away
+      log.debug("no further messages enqueued on probe1 as partition 1 is balanced away")
       probe1.expectNoMessage(500.millis)
 
-      probe2messages should have size (count)
+      probe2messages should have size count
 
       probe1.cancel()
       probe2.cancel()
@@ -211,5 +234,57 @@ class RebalanceSpec extends SpecBase with TestcontainersKafkaLike with Inside {
       control1.isShutdown.futureValue shouldBe Done
       control2.isShutdown.futureValue shouldBe Done
     }
+  }
+}
+
+object AlpakkaAssignor {
+  final val clientIdToPartitionMap = new AtomicReference[Map[String, Set[TopicPartition]]]()
+}
+
+/**
+ * Control the assignment of group members to topic partitions. This requires each consumer to have a distinct
+ * client id so that we can filter them during assignment. The member id is a concatenation of the client id and the
+ * group member instance id that's generated by the Consumer Group coordinator.
+ *
+ * Pass a client.id -> Set[TopicPartition] map to `AlpakkaAssignor.clientIdToPartitionMap` **before** you anticipate a
+ * rebalance to occur in your test.
+ */
+class AlpakkaAssignor extends AbstractPartitionAssignor {
+  val log: Logger = LoggerFactory.getLogger(getClass)
+
+  override def name(): String = "alpakka-test"
+
+  override def assign(
+      partitionsPerTopic: util.Map[String, Integer],
+      subscriptions: util.Map[String, ConsumerPartitionAssignor.Subscription]
+  ): util.Map[String, util.List[TopicPartition]] = {
+    val clientIdToPartitionMap = AlpakkaAssignor.clientIdToPartitionMap.get()
+
+    val mapTps = clientIdToPartitionMap.values.flatten.toSet
+    val subscriptionTps = partitionsPerTopic.asScala.flatMap {
+      case (topic, partitions) => (0 until partitions).map(p => new TopicPartition(topic, p))
+    }.toSet
+
+    val missingFromMap = subscriptionTps.diff(mapTps)
+
+    if (missingFromMap.nonEmpty)
+      throw new Exception(
+        s"Missing the following partition assignments from the static assignment map: $missingFromMap"
+      )
+
+    val assignments = for {
+      memberId <- subscriptions.keySet().asScala
+    } yield {
+      val (_, tps) = clientIdToPartitionMap
+        .find { case (clientId, _) => memberId.startsWith(clientId) }
+        .getOrElse {
+          throw new Exception(s"Couldn't find client id that matches '$memberId' in static assignment map!")
+        }
+      memberId -> tps.toList.asJava
+    }
+
+    log.debug(s"Assignments: $assignments")
+
+    assignments.toMap.asJava
   }
 }
