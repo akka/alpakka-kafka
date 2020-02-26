2f59635a6d6fb9f05db798e9ef2068e9daaea182
Sean Glover <sean@seanglover.com>
2020-02-26 16:47:03 -0500

    Promise message extractor

--- core/src/main/scala/akka/kafka/KafkaClusterSharding.scala
+++ core/src/main/scala/akka/kafka/KafkaClusterSharding.scala
@@ -1,23 +1,27 @@
+/*
+ * Copyright (C) 2014 - 2016 Softwaremill <http://softwaremill.com>
+ * Copyright (C) 2016 - 2019 Lightbend Inc. <http://www.lightbend.com>
+ */
+
 package akka.kafka
 
 import java.util.concurrent.atomic.AtomicInteger
 
-import akka.actor.typed.Behavior
 import akka.actor.typed.scaladsl.adapter._
-import akka.actor.typed.scaladsl.Behaviors
-import akka.actor.{ActorRef, ExtendedActorSystem, Extension, ExtensionId}
+import akka.actor.{Actor, ActorRef, ActorSystem, Address, ExtendedActorSystem, Extension, ExtensionId, Props}
 import akka.annotation.{ApiMayChange, InternalApi}
 import akka.cluster.sharding.external.ExternalShardAllocation
 import akka.cluster.sharding.typed.scaladsl.EntityTypeKey
 import akka.cluster.sharding.typed.{ShardingEnvelope, ShardingMessageExtractor}
 import akka.cluster.typed.Cluster
+import akka.event.Logging
 import akka.kafka
 import akka.kafka.scaladsl.MetadataClient
 import akka.util.Timeout._
 import org.apache.kafka.common.utils.Utils
 
 import scala.concurrent.duration._
-import scala.concurrent.{ExecutionContextExecutor, Future}
+import scala.concurrent.{ExecutionContextExecutor, Future, Promise}
 import scala.util.{Failure, Success}
 
 /**
@@ -26,9 +30,8 @@ import scala.util.{Failure, Success}
 class KafkaClusterSharding(system: ExtendedActorSystem) extends Extension {
   import KafkaClusterSharding._
 
-  private val metadataActorCounter = new AtomicInteger
-  private var _messageExtractor: Option[KafkaShardingMessageExtractor[_]] = None
-  private var _messageExtractorNoEnvelope: Option[KafkaShardingNoEnvelopeExtractor[_]] = None
+  private val _messageExtractor: Promise[KafkaShardingMessageExtractor[_]] = Promise()
+  private val _messageExtractorNoEnvelope: Promise[KafkaShardingNoEnvelopeExtractor[_]] = Promise()
 
   /**
    * API MAY CHANGE
@@ -46,16 +49,17 @@ class KafkaClusterSharding(system: ExtendedActorSystem) extends Extension {
   @ApiMayChange
   def messageExtractor[M](topic: String,
                           timeout: FiniteDuration,
-                          settings: ConsumerSettings[_,_]): Future[KafkaShardingMessageExtractor[M]] = _messageExtractor match {
-    case Some(extractor) => Future.successful(extractor.asInstanceOf[KafkaShardingMessageExtractor[M]])
-    case _ =>
-      getPartitionCount(topic, timeout, settings)
-        .map { kafkaPartitions =>
-          val extractor = new KafkaShardingMessageExtractor[M](kafkaPartitions)
-          _messageExtractor = Some(extractor)
-          extractor
-        }(system.dispatcher)
-  }
+                          settings: ConsumerSettings[_, _]): Future[KafkaShardingMessageExtractor[M]] =
+    if (_messageExtractor.isCompleted)
+      _messageExtractor.future.asInstanceOf[Future[KafkaShardingMessageExtractor[M]]]
+    else {
+      _messageExtractor
+        .tryCompleteWith(
+          getPartitionCount(topic, timeout, settings).map(new KafkaShardingMessageExtractor[M](_))(system.dispatcher)
+        )
+        .future
+        .asInstanceOf[Future[KafkaShardingMessageExtractor[M]]]
+    }
 
   /**
    * API MAY CHANGE
@@ -90,16 +94,20 @@ class KafkaClusterSharding(system: ExtendedActorSystem) extends Extension {
   def messageExtractorNoEnvelope[M](topic: String,
                                     timeout: FiniteDuration,
                                     entityIdExtractor: M => String,
-                                    settings: ConsumerSettings[_,_]): Future[KafkaShardingNoEnvelopeExtractor[M]] =  _messageExtractorNoEnvelope match {
-    case Some(extractor) => Future.successful(extractor.asInstanceOf[KafkaShardingNoEnvelopeExtractor[M]])
-    case _ =>
-      getPartitionCount(topic, timeout, settings)
-        .map { kafkaPartitions =>
-          val extractor = new KafkaShardingNoEnvelopeExtractor[M](kafkaPartitions, entityIdExtractor)
-          _messageExtractorNoEnvelope = Some(extractor)
-          extractor
-        }(system.dispatcher)
-  }
+                                    settings: ConsumerSettings[_, _]): Future[KafkaShardingNoEnvelopeExtractor[M]] =
+    if (_messageExtractorNoEnvelope.isCompleted)
+      _messageExtractorNoEnvelope.future.asInstanceOf[Future[KafkaShardingNoEnvelopeExtractor[M]]]
+    else {
+      _messageExtractorNoEnvelope
+        .tryCompleteWith(
+          getPartitionCount(topic, timeout, settings)
+            .map(partitions => new KafkaShardingNoEnvelopeExtractor[M](partitions, entityIdExtractor))(
+              system.dispatcher
+            )
+        )
+        .future
+        .asInstanceOf[Future[KafkaShardingNoEnvelopeExtractor[M]]]
+    }
 
   /**
    * API MAY CHANGE
@@ -113,21 +121,28 @@ class KafkaClusterSharding(system: ExtendedActorSystem) extends Extension {
    * that entities are routed to the same Entity type.
    */
   @ApiMayChange
-  def messageExtractorNoEnvelope[M](kafkaPartitions: Int, entityIdExtractor: M => String): Future[KafkaShardingNoEnvelopeExtractor[M]] =
+  def messageExtractorNoEnvelope[M](kafkaPartitions: Int,
+                                    entityIdExtractor: M => String): Future[KafkaShardingNoEnvelopeExtractor[M]] =
     Future.successful(new KafkaShardingNoEnvelopeExtractor[M](kafkaPartitions, entityIdExtractor))
 
-  private def getPartitionCount[M](topic: String, timeout: FiniteDuration, settings: ConsumerSettings[_, _]): Future[Int] = {
+  private val metadataConsumerActorNum = new AtomicInteger
+  private def getPartitionCount[M](topic: String,
+                                   timeout: FiniteDuration,
+                                   settings: ConsumerSettings[_, _]): Future[Int] = {
     implicit val ec: ExecutionContextExecutor = system.dispatcher
-    val actorNum = metadataActorCounter.getAndIncrement()
+    val num = metadataConsumerActorNum.getAndIncrement()
     val consumerActor = system
-      .systemActorOf(KafkaConsumerActor.props(settings), s"metadata-consumer-actor-$actorNum")
+      .systemActorOf(KafkaConsumerActor.props(settings), s"metadata-consumer-actor-$num")
     val metadataClient = MetadataClient.create(consumerActor, timeout)
     val numPartitions = metadataClient.getPartitionsFor(topic).map(_.length)
+    numPartitions.onComplete(_ => system.stop(consumerActor))
     numPartitions.map { count =>
       system.log.info("Retrieved {} partitions for topic '{}'", count, topic)
       count
     }
   }
+
+  private val shadingRebalanceListenerActorNum = new AtomicInteger
 
   /**
    * API MAY CHANGE
@@ -141,43 +156,26 @@ class KafkaClusterSharding(system: ExtendedActorSystem) extends Extension {
    */
   @ApiMayChange
   def rebalanceListener(typeKey: EntityTypeKey[_]): ActorRef = {
-    val actor: Behavior[ConsumerRebalanceEvent] = Behaviors.setup { ctx =>
-      val typeKeyName = typeKey.name
-      val shardAllocationClient = ExternalShardAllocation(ctx.system).clientFor(typeKeyName)
-      val address = Cluster(ctx.system).selfMember.address
-      Behaviors.receive[ConsumerRebalanceEvent] {
-        case (ctx, TopicPartitionsAssigned(_, partitions)) =>
-          import ctx.executionContext
-          val partitionsList = partitions.mkString(",")
-          ctx.log.info("Consumer group '{}' is assigning topic partitions to cluster member '{}': [{}]",
-            typeKeyName, address, partitionsList)
-          val updates = partitions.map { tp =>
-            val shardId = tp.partition().toString
-            // Kafka partition number becomes the akka shard id
-            // TODO: support assigning more than 1 shard id at once?
-            shardAllocationClient.updateShardLocation(shardId, address)
-          }
-          Future
-            .sequence(updates)
-            // Each Future returns successfully once a majority of cluster nodes receive the update. There's no point
-            // blocking here because the rebalance listener is triggered asynchronously. If we want to block during
-            // rebalance then we should provide an implementation using the `PartitionAssignmentHandler` instead
-            .onComplete {
-              case Success(_) =>
-                ctx.log.info("Completed consumer group '{}' assignment of topic partitions to cluster member '{}': [{}]",
-                  typeKeyName, address, partitionsList)
-              case Failure(ex) =>
-                ctx.log.error("A failure occurred while updating cluster shards", ex)
-            }
-          Behaviors.same
-        case (_, TopicPartitionsRevoked(_, _)) => Behaviors.same
-      }
-    }
+    val num = shadingRebalanceListenerActorNum.getAndIncrement()
+    system.systemActorOf(Props(new RebalanceListener(typeKey)), s"kafka-cluster-sharding-rebalance-listener-$num")
+  }
 
-    system
-      .toTyped
-      .systemActorOf(actor, "kafka-cluster-sharding-rebalance-listener")
-      .toClassic
+  /**
+   * API MAY CHANGE
+   *
+   * Create an Alpakka Kafka rebalance listener that handles [[TopicPartitionsAssigned]] events. The [[typeKey]] is used
+   * to create the [[ExternalShardAllocation]] client. When partitions are assigned to this consumer group member the
+   * rebalance listener will use the [[ExternalShardAllocation]] client to update the External Sharding strategy
+   * accordingly so that entities are (eventually) routed to the local Akka cluster member.
+   *
+   * Returns an Akka classic [[ActorRef]] that can be passed to an Alpakka Kafka [[ConsumerSettings]].
+   */
+  @ApiMayChange
+  def rebalanceListener(otherSystem: ActorSystem, typeKey: EntityTypeKey[_]): ActorRef = {
+    val num = shadingRebalanceListenerActorNum.getAndIncrement()
+    otherSystem
+      .asInstanceOf[ExtendedActorSystem]
+      .systemActorOf(Props(new RebalanceListener(typeKey)), s"kafka-cluster-sharding-rebalance-listener-$num")
   }
 }
 
@@ -187,23 +185,65 @@ object KafkaClusterSharding extends ExtensionId[KafkaClusterSharding] {
     def kafkaPartitions: Int
     def shardId(entityId: String): String = {
       // simplified version of Kafka's `DefaultPartitioner` implementation
-      val partition = org.apache.kafka.common.utils.Utils.toPositive(Utils.murmur2(entityId.getBytes())) % kafkaPartitions
+      val partition = org.apache.kafka.common.utils.Utils
+          .toPositive(Utils.murmur2(entityId.getBytes())) % kafkaPartitions
       partition.toString
     }
   }
 
   @InternalApi
-  final class KafkaShardingMessageExtractor[M] private[kafka](val kafkaPartitions: Int)
-    extends ShardingMessageExtractor[ShardingEnvelope[M], M] with KafkaClusterShardingContract {
+  final class KafkaShardingMessageExtractor[M] private[kafka] (val kafkaPartitions: Int)
+      extends ShardingMessageExtractor[ShardingEnvelope[M], M]
+      with KafkaClusterShardingContract {
     override def entityId(envelope: ShardingEnvelope[M]): String = envelope.entityId
     override def unwrapMessage(envelope: ShardingEnvelope[M]): M = envelope.message
   }
 
   @InternalApi
-  final class KafkaShardingNoEnvelopeExtractor[M] private[kafka](val kafkaPartitions: Int, entityIdExtractor: M => String)
-    extends ShardingMessageExtractor[M, M] with KafkaClusterShardingContract {
+  final class KafkaShardingNoEnvelopeExtractor[M] private[kafka] (val kafkaPartitions: Int,
+                                                                  entityIdExtractor: M => String)
+      extends ShardingMessageExtractor[M, M]
+      with KafkaClusterShardingContract {
     override def entityId(message: M): String = entityIdExtractor(message)
     override def unwrapMessage(message: M): M = message
+  }
+
+  @InternalApi
+  private[kafka] final class RebalanceListener(typeKey: EntityTypeKey[_]) extends Actor {
+    private val log = Logging(context.system, this)
+    private val typeKeyName = typeKey.name
+    private val shardAllocationClient = ExternalShardAllocation(context.system).clientFor(typeKeyName)
+    private val address: Address = Cluster(context.system.toTyped).selfMember.address
+
+    override def receive: Receive = {
+      case TopicPartitionsAssigned(_, partitions) =>
+        implicit val ec: ExecutionContextExecutor = context.classicActorContext.dispatcher
+        val partitionsList = partitions.mkString(",")
+        log.info("Consumer group '{}' is assigning topic partitions to cluster member '{}': [{}]",
+                 typeKeyName,
+                 address,
+                 partitionsList)
+        val updates = partitions.map { tp =>
+          val shardId = tp.partition().toString
+          // Kafka partition number becomes the akka shard id
+          // TODO: support assigning more than 1 shard id at once?
+          shardAllocationClient.updateShardLocation(shardId, address)
+        }
+        Future
+          .sequence(updates)
+          // Each Future returns successfully once a majority of cluster nodes receive the update. There's no point
+          // blocking here because the rebalance listener is triggered asynchronously. If we want to block during
+          // rebalance then we should provide an implementation using the `PartitionAssignmentHandler` instead
+          .onComplete {
+            case Success(_) =>
+              log.info("Completed consumer group '{}' assignment of topic partitions to cluster member '{}': [{}]",
+                       typeKeyName,
+                       address,
+                       partitionsList)
+            case Failure(ex) =>
+              log.error("A failure occurred while updating cluster shards", ex)
+          }
+    }
   }
 
   override def createExtension(system: ExtendedActorSystem): kafka.KafkaClusterSharding =
