84d86444e98d90cf48f328620290c6d9fb8e2f78
Jesse Yates <jesse.k.yates@gmail.com>
2020-06-17 15:23:31 -0700

    Do not commit offsets for unassigned partitions (#1123)

--- tests/src/test/scala/akka/kafka/scaladsl/RetentionPeriodSpec.scala
+++ tests/src/test/scala/akka/kafka/scaladsl/RetentionPeriodSpec.scala
@@ -5,6 +5,7 @@
 
 package akka.kafka.scaladsl
 
+import java.nio.ByteBuffer
 import java.util.concurrent.ConcurrentLinkedQueue
 
 import akka.Done
@@ -14,6 +15,12 @@ import akka.kafka.testkit.scaladsl.TestcontainersKafkaPerClassLike
 import akka.stream.scaladsl.Keep
 import akka.stream.testkit.scaladsl.StreamTestKit.assertAllStagesStopped
 import akka.stream.testkit.scaladsl.TestSink
+import akka.testkit.TestProbe
+import kafka.common.OffsetAndMetadata
+import kafka.coordinator.group.{GroupMetadataManager, GroupTopicPartition, OffsetKey}
+import org.apache.kafka.clients.consumer.{ConsumerConfig, ConsumerRecord}
+import org.apache.kafka.common.TopicPartition
+import org.apache.kafka.common.serialization.ByteArrayDeserializer
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
@@ -36,8 +43,136 @@ class RetentionPeriodSpec extends SpecBase with TestcontainersKafkaPerClassLike 
       }
     }
 
-  "After retention period (1 min) consumer" must {
+  final val partition1 = 1
+  final val consumerClientId1 = "consumer-1"
+  final val consumerClientId2 = "consumer-2"
+  final val consumerClientId3 = "consumer-3"
 
+  "While refreshing offsets the consumer" must {
+    "not commit or refresh partitions that are not assigned" in assertAllStagesStopped {
+      val topic1 = createTopic(0, partitions = 2)
+      val group1 = createGroupId(1)
+      val tp0 = new TopicPartition(topic1, partition0)
+      val tp1 = new TopicPartition(topic1, partition1)
+      val consumerSettings = consumerDefaults
+        .withProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "1")
+        .withProperty(ConsumerConfig.PARTITION_ASSIGNMENT_STRATEGY_CONFIG, classOf[AlpakkaAssignor].getName)
+        .withCommitRefreshInterval(1.seconds)
+        .withGroupId(group1)
+
+      awaitProduce(produce(topic1, 0 to 20, partition0))
+
+      AlpakkaAssignor.clientIdToPartitionMap.set(Map(consumerClientId1 -> Set(tp0, tp1)))
+
+      log.debug("Subscribe to the topic (without demand)")
+      val probe1rebalanceActor = TestProbe()
+      val probe1subscription = Subscriptions.topics(topic1).withRebalanceListener(probe1rebalanceActor.ref)
+      val (control1, probe1) = Consumer
+        .committableSource(consumerSettings.withClientId(consumerClientId1), probe1subscription)
+        .toMat(TestSink.probe)(Keep.both)
+        .run()
+
+      log.debug("Await initial partition assignment")
+      probe1rebalanceActor.expectMsg(TopicPartitionsAssigned(probe1subscription, Set(tp0, tp1)))
+
+      log.debug("read one message from probe1 with partition 0")
+      val firstOffset = probe1.requestNext()
+      firstOffset.record.topic() should be(topic1)
+      firstOffset.record.partition() should equal(partition0)
+
+      log.debug("move the partition to the other consumer")
+      AlpakkaAssignor.clientIdToPartitionMap.set(Map(consumerClientId1 -> Set(tp1), consumerClientId2 -> Set(tp0)))
+
+      log.debug("Subscribe to the topic (without demand)")
+      val probe2rebalanceActor = TestProbe()
+      val probe2subscription = Subscriptions.topics(topic1).withRebalanceListener(probe2rebalanceActor.ref)
+      val (control2, probe2) = Consumer
+        .committableSource(consumerSettings.withClientId(consumerClientId2), probe2subscription)
+        .toMat(TestSink.probe)(Keep.both)
+        .run()
+
+      log.debug("Await a revoke to consumer 1")
+      probe1rebalanceActor.expectMsg(TopicPartitionsRevoked(probe1subscription, Set(tp0, tp1)))
+
+      log.debug("the rebalance finishes")
+      probe1rebalanceActor.expectMsg(TopicPartitionsAssigned(probe1subscription, Set(tp1)))
+      probe2rebalanceActor.expectMsg(TopicPartitionsAssigned(probe2subscription, Set(tp0)))
+
+      // this should setup the refreshing offsets on consumer 1 to go backwards
+      log.debug("committing progress on first consumer for {}, after it has been rebalanced away", tp0)
+      firstOffset.committableOffset.commitInternal()
+
+      log.debug("Resume polling on second consumer, committing progress forward for {}", tp0)
+      val offsets = probe2.request(2).expectNextN(2)
+      for (offset <- offsets) {
+        offset.record.topic() should be(topic1)
+        offset.record.partition() should equal(partition0)
+        offset.committableOffset.commitInternal()
+      }
+
+      sleep(5.seconds, "Wait for a number of offset refreshes")
+
+      // read the __consumer_offsets topic and we should see the flipping back and forth
+      val group2 = createGroupId(2)
+      val group2consumerSettings: ConsumerSettings[Array[Byte], Array[Byte]] = consumerDefaults(
+        new ByteArrayDeserializer(),
+        new ByteArrayDeserializer()
+      ).withProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "1").withGroupId(group2)
+      val probe3subscription = Subscriptions.topics("__consumer_offsets")
+      val (control3, probe3) = Consumer
+        .plainSource(group2consumerSettings.withClientId(consumerClientId3), probe3subscription)
+        .toMat(TestSink.probe)(Keep.both)
+        .run()
+      val commits: Seq[ConsumerRecord[Array[Byte], Array[Byte]]] = probe3.request(100).expectNextN(10)
+
+      // helper method ripped from GroupMetadataManager's formatter
+      def readOffset(
+          consumerRecord: ConsumerRecord[Array[Byte], Array[Byte]]
+      ): Option[(GroupTopicPartition, Option[OffsetAndMetadata])] = {
+        val offsetKey = GroupMetadataManager.readMessageKey(ByteBuffer.wrap(consumerRecord.key())) // Only read the message if it is an offset record.
+        // We ignore the timestamp of the message because GroupMetadataMessage has its own timestamp.
+        offsetKey match {
+          case offsetKey: OffsetKey =>
+            val groupTopicPartition = offsetKey.key
+            val value = consumerRecord.value
+            val formattedValue =
+              if (value == null) None else Some(GroupMetadataManager.readOffsetMessageValue(ByteBuffer.wrap(value)))
+            Some((groupTopicPartition, formattedValue))
+              .asInstanceOf[Option[(GroupTopicPartition, Option[OffsetAndMetadata])]]
+          case _: Any => None
+        }
+      }
+
+      log.debug("Ensure that commits progress in-order")
+      var progress = -1L
+      for (consumerRecord <- commits) {
+        readOffset(consumerRecord) match {
+          case Some((group, offset)) =>
+            log.debug("Committed {}: {}", group: Any, offset: Any)
+            offset match {
+              case Some(position) =>
+                if (group.topicPartition.equals(tp0)) {
+                  position.offset shouldBe >=(progress)
+                  progress = position.offset
+                }
+              case None => //noop
+            }
+          case None => // noop
+        }
+      }
+
+      // cleanup
+      probe1.cancel()
+      probe2.cancel()
+      probe3.cancel()
+
+      control1.isShutdown.futureValue shouldBe Done
+      control2.isShutdown.futureValue shouldBe Done
+      control3.isShutdown.futureValue shouldBe Done
+    }
+  }
+
+  "After retention period (1 min) consumer" must {
     "resume from committed offset" in assertAllStagesStopped {
       val topic1 = createTopic()
       val group1 = createGroupId()
