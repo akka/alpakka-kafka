ee32cbbf5e359e7e89b7ddb8ce89ca3988018a15
Sean Glover <sean@seanglover.com>
2020-08-11 16:15:04 -0400

    deprecation msg

--- core/src/main/scala/akka/kafka/RestrictedConsumer.scala
+++ core/src/main/scala/akka/kafka/RestrictedConsumer.scala
@@ -5,6 +5,8 @@
 
 package akka.kafka
 
+import java.util
+
 import akka.annotation.ApiMayChange
 import com.github.ghik.silencer.silent
 import org.apache.kafka.clients.consumer.{Consumer, OffsetAndMetadata, OffsetAndTimestamp}
@@ -38,7 +40,14 @@ final class RestrictedConsumer(consumer: Consumer[_, _], duration: java.time.Dur
    * See [[org.apache.kafka.clients.consumer.KafkaConsumer#committed(TopicPartition,java.time.Duration)]]
    */
   @silent
+  @deprecated("use `committed(java.util.Set[TopicPartition])`", "2.1.0")
   def committed(tp: TopicPartition): OffsetAndMetadata = consumer.committed(tp, duration)
+
+  /**
+   * See [[org.apache.kafka.clients.consumer.KafkaConsumer#committed(java.util.Set[TopicPartition],java.time.Duration)]]
+   */
+  def committed(partitions: util.Set[TopicPartition]): util.Map[TopicPartition, OffsetAndMetadata] =
+    consumer.committed(partitions, duration)
 
   /**
    * See [[org.apache.kafka.clients.consumer.KafkaConsumer#endOffsets(java.util.Collection[TopicPartition],java.time.Duration)]]
