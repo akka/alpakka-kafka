eb45e18cd321397e4133d8ce715b6a719e359b90
Patrik Nordwall <patrik.nordwall@gmail.com>
2023-10-06 15:41:30 +0200

    build: Akka 2.9.3-M3 and publish to Akka repo

---
+++ project/Publish.scala
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2023 Lightbend Inc. <https://www.lightbend.com>
+ */
+package akka
+
+import java.util.concurrent.atomic.AtomicBoolean
+
+import scala.language.postfixOps
+
+import sbt.{Def, _}
+import Keys._
+import com.geirsson.CiReleasePlugin
+import com.jsuereth.sbtpgp.PgpKeys.publishSigned
+import xerial.sbt.Sonatype.autoImport.sonatypeProfileName
+
+/**
+ * For projects that are not published.
+ */
+object NoPublish extends AutoPlugin {
+  override def requires = plugins.JvmPlugin
+
+  override def projectSettings = Seq(
+    publish / skip := true,
+    publishArtifact := false,
+    publish := {},
+    publishLocal := {}
+  )
+}
+
+object Publish extends AutoPlugin {
+  override def requires = plugins.JvmPlugin
+  override def trigger = AllRequirements
+
+  lazy val beforePublishTask = taskKey[Unit]("setup before publish")
+
+  lazy val beforePublishDone = new AtomicBoolean(false)
+
+  def beforePublish(snapshot: Boolean) = {
+    if (beforePublishDone.compareAndSet(false, true)) {
+      CiReleasePlugin.setupGpg()
+      if (!snapshot)
+        cloudsmithCredentials(validate = true)
+    }
+  }
+
+  override def projectSettings: Seq[Def.Setting[_]] = Seq(
+    sonatypeProfileName := "com.typesafe",
+    beforePublishTask := beforePublish(isSnapshot.value),
+    publishSigned := publishSigned.dependsOn(beforePublishTask).value,
+    publishTo := (if (isSnapshot.value)
+                    Some(Resolver.file("file", target.value / "repository")) // FIXME snapshot repo
+                  else
+                    Some("Cloudsmith API".at("https://maven.cloudsmith.io/lightbend/akka/"))),
+    credentials ++= (if (isSnapshot.value) Seq[Credentials]() else cloudsmithCredentials(validate = false))
+  )
+
+  def cloudsmithCredentials(validate: Boolean): Seq[Credentials] = {
+    (sys.env.get("PUBLISH_USER"), sys.env.get("PUBLISH_PASSWORD")) match {
+      case (Some(user), Some(password)) =>
+        Seq(Credentials("Cloudsmith API", "maven.cloudsmith.io", user, password))
+      case _ =>
+        if (validate)
+          throw new Exception("Publishing credentials expected in `PUBLISH_USER` and `PUBLISH_PASSWORD`.")
+        else
+          Nil
+    }
+  }
+}
